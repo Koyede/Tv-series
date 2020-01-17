56e5f2b710cdfd050bc83923b46d56b8cea5721b
Koyede Ladoye <flexwithlado@gmail.com>
2020-01-17 11:18:50 +0100

    another change

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
+import loader from "../../Loader";
 
 class SingleSeries extends Component {
+  state = {
+    show: null
+  };
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+      .then(Response => Response.json())
+      .then(json => this.setState({ show: json }));
+  }
+
   render() {
+    const { show } = this.state;
+    console.log(show);
+
     return (
       <div>
-        <p>Single series - the show id will be {this.props.match.params.id} </p>
+        {show === null && <loader />}
+        {show !== null && (
+          <div>
+            <p>{show.name}</p>
+            <p>Premiered - {show.premiered}</p>
+            <p>Ratings - {show.rating.average}</p>
+            <p>Episodes - {show._embedded.episodes.length}</p>
+            <p>
+              <img alt="show" src={show.image.medium} />
+            </p>
+          </div>
+        )}
       </div>
     );
   }
